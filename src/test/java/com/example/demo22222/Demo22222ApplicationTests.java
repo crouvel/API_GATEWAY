901a105e82fc5b9baaf39896b04780358d6d71f1
crouvel <55972660+crouvel@users.noreply.github.com>
2022-11-27 21:07:17 +0100

    Initial commit

---
+++ src/test/java/com/example/demo22222/Demo22222ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo22222;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo22222ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
