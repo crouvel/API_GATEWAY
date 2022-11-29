a6ae6d9f79ee3939900ff485dfe0476bcfa489f9
crouvel <55972660+crouvel@users.noreply.github.com>
2022-11-30 00:34:23 +0100

    api gateway working

--- src/main/java/com/example/demo22222/Demo22222Application.java
+++ src/main/java/com/example/demo22222/Demo22222Application.java
@@ -2,12 +2,21 @@ package com.example.demo22222;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Demo22222Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Demo22222Application.class, args);
+    }
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        System.out.println(" === ROUTE === " + builder);
+        return builder.routes().build();
     }
 
 }
